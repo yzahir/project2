bd3a635c6b95a8f5da9bbb128a2b669dd8a007b5
BilouBrot <github@traut.men>
2025-06-16 17:10:14 +0200

    Add socket import and print hostname in main loop

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 from pipuck.pipuck import PiPuck
 import random
 import math
+import socket
 
 # Define variables and callbacks
 Broker = "192.168.178.56"  # Replace with your broker address
@@ -173,6 +174,7 @@ current_state = STATE_START
 try:
     for _ in range(1000):
         # TODO: Do your stuff here
+        print (f'Hostname: {socket.gethostname()}')
         print(f'puck_dict: {puck_dict}')
         x, y, angle = get_position()
         if x is not None and y is not None:
